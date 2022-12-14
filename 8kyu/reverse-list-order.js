9470fced71c368cbb3e3678aa213c684fc2e4a0d
Fatima Basharat <fatima8746@gmail.com>
2022-12-14 11:07:39 -0500

    reverse list order

---
+++ 8kyu/reverse-list-order.js
@@ -0,0 +1,5 @@
+// DESCRIPTION:
+// In this kata you will create a function that takes in a list and returns a list with the reverse order.
+
+// My solution
+const reverseList = list => list.reverse();
