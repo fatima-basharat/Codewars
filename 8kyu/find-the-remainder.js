a72043325f8cccf9bcf9544d8b9f4c9cf93d66a6
Fatima Basharat <fatima8746@gmail.com>
2022-12-15 14:06:11 -0500

    finding the remainder

---
+++ 8kyu/find-the-remainder.js
@@ -0,0 +1,7 @@
+// Description
+// Write a function that accepts two integers and returns the remainder of dividing the larger value by the smaller value.
+
+// My solution
+function remainder(n, m){
+    return n > m ? n % m : m % n;
+  }
