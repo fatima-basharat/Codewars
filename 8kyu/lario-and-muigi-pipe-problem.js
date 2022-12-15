eadcdbae18fd6c65806dbd261773dd47a6e2bbc6
Fatima Basharat <fatima8746@gmail.com>
2022-12-15 14:23:16 -0500

    returns list with vals incremented by 1 for each index

---
+++ 8kyu/lario-and-muigi-pipe-problem.js
@@ -0,0 +1,14 @@
+// Task
+// Given the a list of numbers, return a fixed list so that the values increment by 1 for each index from the minimum value up to the maximum value (both included).
+
+// My solution
+function pipeFix(numbers){
+    let first = numbers[0];
+    let last = numbers[numbers.length-1];
+    
+    let arr = [];
+    for(let i = first; i<=last; i++){
+      arr.push(i)
+    }
+    return arr
+  }
