fb696d635cc5f5d3313c6cf786ce118bccd7009b
Fatima Basharat <fatima8746@gmail.com>
2022-12-16 12:26:05 -0500

    take in object and return string including certain properties

---
+++ 8kyu/basic-data-types-object.js
@@ -0,0 +1,10 @@
+// Description:
+// Given a function animal, accept 1 parameter:obj like this:
+// {name:"dog",legs:4,color:"white"}
+// and return a string like this:
+// "This white dog has 4 legs."
+
+// My solution
+function animal(obj){
+    return `This ${obj.color} ${obj.name} has ${obj.legs} legs.`;
+}
