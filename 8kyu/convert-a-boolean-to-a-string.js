13a7590786598e5b545959bd7a596cdf065099a8
Fatima Basharat <fatima8746@gmail.com>
2022-12-20 12:08:33 -0500

    returns string of given boolean

---
+++ 8kyu/convert-a-boolean-to-a-string.js
@@ -0,0 +1,6 @@
+// Description
+// Implement a function which convert the given boolean value into its string representation.
+// My solution
+function booleanToString(b){
+    return b.toString();
+}
