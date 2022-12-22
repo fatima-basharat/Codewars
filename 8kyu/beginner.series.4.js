0049bea446751d7061d456d8b66295a027c1a7ed
Fatima Basharat <fatima8746@gmail.com>
2022-12-22 11:52:06 -0500

    returns speed in cm per second given rounded down

---
+++ 8kyu/beginner.series.4.js
@@ -0,0 +1,7 @@
+// Description
+// The cockroach is one of the fastest insects. Write a function which takes its speed in km per hour and returns it in cm per second, rounded down to the integer (= floored).
+
+// My solution
+function cockroachSpeed(s) {
+    return Math.floor(s/0.036);
+}
