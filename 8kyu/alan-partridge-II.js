2d0531bcaa8721920aa3e9d71d81b4a49d3f1d44
Fatima Basharat <fatima8746@gmail.com>
2022-12-31 11:37:14 -0500

    returns string if x meets test case

---
+++ 8kyu/alan-partridge-II.js
@@ -0,0 +1,11 @@
+// Description
+// Your job is simple, if x squared is more than 1000, return It's hotter than the sun!!, else, return Help yourself to a honeycomb Yorkie for the glovebox.
+
+// My solution
+function apple(x){
+    if(x * x > 1000){
+      return 'It\'s hotter than the sun!!'
+    }else{
+      return 'Help yourself to a honeycomb Yorkie for the glovebox.'
+    }
+}
