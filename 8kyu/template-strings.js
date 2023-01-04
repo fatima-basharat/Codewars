395005f2d32a4ca2d41c96661c329325d8e3b859
Fatima Basharat <fatima8746@gmail.com>
2023-01-04 13:58:47 -0500

    returns correct string using template string feature

---
+++ 8kyu/template-strings.js
@@ -0,0 +1,8 @@
+// Description
+// Your task is to return the correct string using the Template String Feature.
+// You must output a string containing the two strings with the word ```' are '```
+
+// My solution
+var TempleStrings = function(obj, feature) {
+    return `${obj} are ${feature}`;
+}
