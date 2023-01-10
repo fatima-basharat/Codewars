0c313ef8b3409c3d1c1d58f99dcefa9bca1934ca
Fatima Basharat <fatima8746@gmail.com>
2023-01-10 13:17:51 -0500

    returns hello, name or says hello, world

---
+++ 8kyu/hello-name-or-world.js
@@ -0,0 +1,8 @@
+// Description
+// Define a method hello that returns "Hello, Name!" to a given name, or says Hello, World! if name is not given (or passed as an empty String).
+// Assuming that name is a String and it checks for user typos to return a name with a first capital letter (Xxxx
+
+// My solution
+function hello(name) {
+    return `Hello, ${name ? name[0].toUpperCase() + name.slice(1).toLowerCase() : "World"}!`
+}
